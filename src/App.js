2a050ba43d8618f35741e8ef89dbd6b4fcdc42aa
Cryptic <reevesplayer@outlook.com>
2024-01-16 17:56:30 -0500

    Removed Testing files; Imported TailwindCSS; added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import HomePage from './components/HomePage';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div>
+      <NavBar />
+      <HomePage />
       <h1>Intern Tracker</h1>
     </div>
+    
   );
 }
 
